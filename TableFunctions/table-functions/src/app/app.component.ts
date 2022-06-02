c1faa1327b3ab70f68398b97f28d46b4967a9fcc
Gautam Balamurali <gtmbalamurali@gmail.com>
2022-06-02 23:33:45 +0530

    Angular skeleton integration for Part 3 Table Functions

--- TableFunctions/table-functions/src/app/app.component.ts
+++ TableFunctions/table-functions/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+/**
+ * Root Component for the entire application
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'table-functions';
